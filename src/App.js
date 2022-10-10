047bd174bd3e5393e108a693bb4f22c42b23491e
FoysalBhuiyann <foysalbhuiyandev@gmail.com>
2022-10-11 01:16:14 +0600

    dinamic nav

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Products from './components/Products/Products';
+import Main from './layout/Main';
+import Friends from './components/Friends/Friends';
 
 function App() {
   const router = createBrowserRouter([
-{ path: '/home', element: <Home></Home>},
-{ path: '/about', element: <About></About>},
-{ path: '/', element: <div>This is Default Page</div>},
-{ path: 'products', element: <Products></Products>}
+    {
+      path: '/',
+      element: <Main></Main>,
+      children:
+        [
+          { path: '/', element: <Home></Home> },
+          { path: 'home', element: <Home></Home> },
+          { path: 'friends', element: <Friends></Friends> },
+          { path: 'about', element: <About></About> }
+        ]
+    },
+    { path: 'products', element: <Products></Products> },
   ])
   return (
     <div className="App">
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import Header from '../Header/Header';
 
 const About = () => {
     return (
         <div>
-            <Header></Header>
             <h3>This is full About Section</h3>
         </div>
     );
